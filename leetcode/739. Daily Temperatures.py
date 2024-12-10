2ed909b2a9016213a505f9c646bd1614d96136e5
inna030 <52088543+inna030@users.noreply.github.com>
2024-12-10 00:33:33 -0500

    Create 739. Daily Temperatures.py
    https://www.loom.com/share/0bac2e37cfcd4d4c86e16900973262c5
    This algorithm uses a monotonic stack to track which days are still waiting for warmer temperatures, ensuring O(N) time complexity.
    For each temperature, if it’s warmer than previous days, the program calculates the wait time for those days, updates the result, and removes those days from the stack.
    Soldiers (temperatures) that never see a warmer day in the future get a 0 in the final answer.

---
+++ leetcode/739. Daily Temperatures.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack=[]
+        n=len(temperatures)
+        res=[0]*n
+        for i in range(n):
+            while stack and temperatures[stack[-1]]<temperatures[i]:
+                prev=stack.pop()
+                res[prev]=i-prev
+            stack.append(i)
+        return res
+
